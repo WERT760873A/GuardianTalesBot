0bff7627fa52c64e255d0daa93376008625525bb
CaiJi <jvfsdfkfwbdsbwsdj@foxmail.com>
2021-05-04 20:54:40 +0800

    .....

--- modules/gacha/__init__.py
+++ modules/gacha/__init__.py
@@ -6,13 +6,13 @@ from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
 
 
-GuardianTalesGacha_10 = on_command("坎游抽卡",aliases=set(["坎游十连"]) ,rule=to_me(), priority=1)
+GuardianTalesGacha_10 = on_command("坎游抽卡",aliases={"坎游十连","十连"} ,rule=to_me(), priority=1)
 
 
 @GuardianTalesGacha_10.handle()
 async def _(bot: Bot, event: Event):
     mes = Gacha().gacha_10()
-    await GuardianTalesGacha_10.finish(message=mes)
+    await GuardianTalesGacha_10.send(message=mes)
 
 
 
