ad5846c9ac727c4c2912901770c2d7d7c5a27500
CaiJi <jvfsdfkfwbdsbwsdj@foxmail.com>
2021-05-04 21:39:41 +0800

    .....

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from nonebot.adapters.cqhttp import Bot as CQHTTPBot
 nonebot.init()
 driver = nonebot.get_driver()
 driver.register_adapter("cqhttp", CQHTTPBot)
-# nonebot.load_builtin_plugins()
+nonebot.load_builtin_plugins()
 
 nonebot.load_plugins("modules")
 
